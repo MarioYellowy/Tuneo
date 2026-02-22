84cb3926dfcfaad3d5780605efde07869f0b497a
YellowDev <111405389+MarioYellowy@users.noreply.github.com>
2026-02-22 01:17:14 -0600

    Fix function signature formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tuneo_ui::slint::{self, ComponentHandle};
 
-fn main()  -> Result<(), slint::PlatformError>{
+fn main() -> Result<(), slint::PlatformError>{
     let app = tuneo_ui::MainWindow::new().expect("Cannot start window");
 
     app.run()
